493483939c31ec44965967acb57f434f0d4adf13
Teemu Tontti <teemu.tontti.tt@gmail.com>
2023-10-09 00:00:25 +0300

    src: Add Amount-class constructor and first tests.
    In src/main/java/Amount.java:
    * Added constructor
    * Added formWholeAmount -function
    
    In src/test/java/TestAmount.java:
    * Add constructor tests
        - testEurosOverTenThousand
        - testEurosUnderZero
        - testCentsOverHundred
        - testCentsUnderZero
        - testCorrectAmount
    * Add formWholeAmount tests
        - testGetWholeAmountCentsOverNine
        - testGetWholeAmountCentsUnderTen

--- src/test/java/TestAmount.java
+++ src/test/java/TestAmount.java
@@ -1,6 +1,42 @@
 package test.java;
+
 import org.junit.*;
+import main.java.Amount;
 
 public class TestAmount {
 
+    // Constructor tests
+    @Test(expected = IllegalArgumentException.class)
+    public void testEurosOverTenThousand() {
+        new Amount(10001,0);
+    }
+    @Test(expected = IllegalArgumentException.class)
+    public void testEurosUnderZero() {
+        new Amount(-1,0);
+    }
+    @Test(expected = IllegalArgumentException.class)
+    public void testCentsOverHundred() {
+        new Amount(1,100);
+    }
+    @Test(expected = IllegalArgumentException.class)
+    public void testCentsUnderZero() {
+        new Amount(1,-1);
+    }
+    @Test
+    public void testCorrectAmount() {
+        Amount amount = new Amount(1,99);
+        Assert.assertNotNull(amount);
+    }
+
+    // formWholeAmount() tests
+    @Test
+    public void testGetWholeAmountCentsOverNine() {
+        Amount amount = new Amount(1, 10);
+        Assert.assertEquals("1.10 €", amount.formWholeAmount(1,10));
+    }
+    @Test
+    public void testGetWholeAmountCentsUnderTen() {
+        Amount amount = new Amount(1, 9);
+        Assert.assertEquals("1.09 €", amount.formWholeAmount(1,9));
+    }
 }
