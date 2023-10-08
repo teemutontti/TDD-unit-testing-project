9bafdce5d917c3ffeba66c432f31332acb25c370
Teemu Tontti <teemu.tontti.tt@gmail.com>
2023-10-09 02:46:35 +0300

    ../../java/TestCategory.java: Add constructor.
    This reverts commit fcad9d6b1adeeea99132f595293ba565845e9ea1.

--- src/test/java/TestCategory.java
+++ src/test/java/TestCategory.java
@@ -3,20 +3,6 @@ package test.java;
 import org.junit.*;
 import main.java.Category;
 
-public class TestCategory {
+class TestCategory {
 
-    // Constructor tests
-    @Test(expected = IllegalArgumentException.class)
-    public void testCategoryConstructorTooLongName() {
-        new Category("12345678901234567890",true);
-    }
-    @Test(expected = IllegalArgumentException.class)
-    public void testCategoryConstructorNoName() {
-        new Category("",true);
-    }
-    @Test
-    public void testCategoryConstructorNotNull() {
-        Category category = new Category("Test", false);
-        Assert.assertNotNull(category);
-    }
 }
