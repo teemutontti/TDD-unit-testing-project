fcad9d6b1adeeea99132f595293ba565845e9ea1
Teemu Tontti <teemu.tontti.tt@gmail.com>
2023-10-09 02:34:20 +0300

    ../../java/TestCategory.java: Add constructor.
    Add constructor which includes varibles:
    * String name; The category's name, can't be null and exceed limitation
    * boolean isNeed; Is category considered a need, if not it's considered
    as a want
    * ArrayList<String> transactions; Holds all transactions in the
    category (will be changed to use Transaction instead of String)
    * int nameLengthLimit; The acceptable limit for name length
    
    Constructor throws an error if the length of the name is over the
    nameLengthLimit or lesser than zero.

--- src/test/java/TestCategory.java
+++ src/test/java/TestCategory.java
@@ -3,6 +3,20 @@ package test.java;
 import org.junit.*;
 import main.java.Category;
 
-class TestCategory {
+public class TestCategory {
 
+    // Constructor tests
+    @Test(expected = IllegalArgumentException.class)
+    public void testCategoryConstructorTooLongName() {
+        new Category("12345678901234567890",true);
+    }
+    @Test(expected = IllegalArgumentException.class)
+    public void testCategoryConstructorNoName() {
+        new Category("",true);
+    }
+    @Test
+    public void testCategoryConstructorNotNull() {
+        Category category = new Category("Test", false);
+        Assert.assertNotNull(category);
+    }
 }
