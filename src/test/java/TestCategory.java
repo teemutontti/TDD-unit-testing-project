6dbec40b195775d7a4b2f6f452fab6a3143acfd7
Teemu Tontti <teemu.tontti.tt@gmail.com>
2023-10-09 03:06:54 +0300

    ../../java/TestCategory.java: Add getter tests.

--- src/test/java/TestCategory.java
+++ src/test/java/TestCategory.java
@@ -7,16 +7,28 @@ public class TestCategory {
 
     // Constructor
     @Test
-    public void testCategoryConstructor() {
+    public void testConstructor() {
         Category category = new Category("Name", true);
         Assert.assertNotNull(category);
     }
     @Test(expected = IllegalArgumentException.class)
-    public void testCategoryNameTooLong() {
+    public void testNameTooLong() {
         new Category("12345678901234567890",true);
     }
     @Test(expected = IllegalArgumentException.class)
-    public void testCategoryNameTooShort() {
+    public void testNameTooShort() {
         new Category("",true);
+    }
+
+    // Getter tests
+    @Test
+    public void testGetName() {
+        Category category = new Category("test", false);
+        Assert.assertEquals("test", category.getName());
+    }
+    @Test
+    public void testGetIsNeed() {
+        Category category = new Category("test", false);
+        Assert.assertEquals(false, category.getIsNeed());
     }
 }
