e6263c2e285dd4d3de7ccc1bd06d390e16905fd9
Teemu Tontti <teemu.tontti.tt@gmail.com>
2023-10-09 03:01:04 +0300

    ../../java/TestCategory.java: Add more constructor tests.

--- src/test/java/TestCategory.java
+++ src/test/java/TestCategory.java
@@ -11,4 +11,12 @@ public class TestCategory {
         Category category = new Category("Name", true);
         Assert.assertNotNull(category);
     }
+    @Test(expected = IllegalArgumentException.class)
+    public void testCategoryNameTooLong() {
+        new Category("12345678901234567890",true);
+    }
+    @Test(expected = IllegalArgumentException.class)
+    public void testCategoryNameTooShort() {
+        new Category("",true);
+    }
 }
