c9e84b69db84a18e9d1d6fcbfa420d7b907b52f2
Teemu Tontti <teemu.tontti.tt@gmail.com>
2023-10-09 03:13:59 +0300

    ../../java/TestCategory.java: Add setter tests.

--- src/test/java/TestCategory.java
+++ src/test/java/TestCategory.java
@@ -31,4 +31,20 @@ public class TestCategory {
         Category category = new Category("test", false);
         Assert.assertEquals(false, category.getIsNeed());
     }
+
+    // Setter tests
+    @Test
+    public void testSetName() {
+        Category category = new Category("test1", false);
+        Assert.assertEquals("test1", category.getName());
+        category.setName("test2");
+        Assert.assertEquals("test2", category.getName());
+    }
+    @Test
+    public void testSetIsNeed() {
+        Category category = new Category("test", true);
+        Assert.assertEquals(true, category.getIsNeed());
+        category.setIsNeed(false);
+        Assert.assertEquals(false, category.getIsNeed());
+    }
 }
