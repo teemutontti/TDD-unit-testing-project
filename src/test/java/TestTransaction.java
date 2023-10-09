4b4a293f97901bc2f6d4066c2c68a5ff026f4dbb
Teemu Tontti <teemu.tontti.tt@gmail.com>
2023-10-09 03:54:05 +0300

    ../../java/TestTransaction.java: Edit constructor test.

--- src/test/java/TestTransaction.java
+++ src/test/java/TestTransaction.java
@@ -10,7 +10,7 @@ public class TestTransaction {
     // Constructor
     @Test
     public void testConstructor() {
-        Transaction transaction = new Transaction();
+        Transaction transaction = new Transaction(1, 5, false);
         Assert.assertNotNull(transaction);
     }
 }
