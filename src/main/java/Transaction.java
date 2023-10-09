651ebfde071345014c7615caf1d3bd93ed9a60ce
Teemu Tontti <teemu.tontti.tt@gmail.com>
2023-10-09 03:54:55 +0300

    ../../java/Transaction.java: Add constructor overloading.

--- src/main/java/Transaction.java
+++ src/main/java/Transaction.java
@@ -1,5 +1,21 @@
 package main.java;
 
-public class Transaction {
+import main.java.Amount;
+import main.java.Category;
 
+public class Transaction {
+    private Amount amount;
+    private Category category;
+    private boolean isIncome;
+    private String date;
+
+    // Constructor
+    public Transaction(int euros, int cents, boolean isIncome) {
+        this.amount = new Amount(euros, cents);
+        this.category = null;
+    }
+    public Transaction(int euros, int cents, boolean isIncome, String category) {
+        this.amount = new Amount(euros, cents);
+        this.category = null; // TODO: Check if category is created
+    }
 }
