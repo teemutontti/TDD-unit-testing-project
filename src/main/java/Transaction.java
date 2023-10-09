9c627b8192aaf34b851e068ef952d0a6a8885232
Teemu Tontti <teemu.tontti.tt@gmail.com>
2023-10-09 04:04:03 +0300

    ../../java/Transaction.java: Add formDate function.

--- src/main/java/Transaction.java
+++ src/main/java/Transaction.java
@@ -1,7 +1,8 @@
 package main.java;
-
 import main.java.Amount;
 import main.java.Category;
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
 
 public class Transaction {
     private Amount amount;
@@ -17,5 +18,12 @@ public class Transaction {
     public Transaction(int euros, int cents, boolean isIncome, String category) {
         this.amount = new Amount(euros, cents);
         this.category = null; // TODO: Check if category is created
+    }
+
+    // Form current date
+    public String formDate() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+        LocalDateTime now = LocalDateTime.now();
+        return formatter.format(now);
     }
 }
