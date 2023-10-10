95f69cb5c5532df717a3be9d91394700f2ae5cca
Teemu Tontti <teemu.tontti.tt@gmail.com>
2023-10-11 00:17:14 +0300

    ../../java/TestBudget.java: Add getter tests.
    Added:
    * testGetBalance()
    * testGetIncome()
    * testGetExpense()
    * testGetName()
    * testGetMonth()
    * testGetGoal()
    * testGetTransactions()

--- src/test/java/TestBudget.java
+++ src/test/java/TestBudget.java
@@ -25,4 +25,34 @@ public class TestBudget {
         Budget budget = new Budget("My Budget", "Lokakuu", new Amount(100,0));
         Assert.assertEquals("0.00 €", budget.getBalance());
     }
+    @Test
+    public void testGetIncome() {
+        Budget budget = new Budget("My Budget", "Lokakuu", new Amount(100,0));
+        Assert.assertEquals("0.00 €", budget.getIncome());
+    }
+    @Test
+    public void testGetExpense() {
+        Budget budget = new Budget("My Budget", "Lokakuu", new Amount(100,0));
+        Assert.assertEquals("0.00 €", budget.getExpense());
+    }
+    @Test
+    public void testGetName() {
+        Budget budget = new Budget("My Budget", "Lokakuu", new Amount(100,0));
+        Assert.assertEquals("My Budget", budget.getName());
+    }
+    @Test
+    public void testGetMonth() {
+        Budget budget = new Budget("My Budget", "Lokakuu", new Amount(100,0));
+        Assert.assertEquals("Lokakuu", budget.getMonth());
+    }
+    @Test
+    public void testGetGoal() {
+        Budget budget = new Budget("My Budget", "Lokakuu", new Amount(100,0));
+        Assert.assertEquals("100.00 €", budget.getGoal());
+    }
+    @Test
+    public void testGetTransactions() {
+        Budget budget = new Budget("My Budget", "Lokakuu", new Amount(100, 0));
+        Assert.assertEquals("No transactions found!", budget.getTransactions());
+    }
 }
