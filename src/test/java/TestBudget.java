e085b8614374b7167476acaf3a0e72c877385579
Teemu Tontti <teemu.tontti.tt@gmail.com>
2023-10-10 22:22:55 +0300

    ../../java/TestBudget.java: (testConstructorNotNull): Add more variables.

--- src/test/java/TestBudget.java
+++ src/test/java/TestBudget.java
@@ -1,14 +1,25 @@
 package test.java;
 
 import org.junit.*;
+import java.util.*;
 import main.java.Budget;
+import main.java.Amount;
+import main.java.Transaction;
 
 public class TestBudget {
 
     // Constructor tests
     @Test
-    public void testConstructor() {
-        Budget budget = new Budget();
+    public void testConstructorNotNull() {
+        String budgetName = "My First Budget";
+        String month = "Lokakuu";
+        Amount income = new Amount(0, 0);
+        Amount expense = new Amount(0, 0);
+        Amount balance = new Amount(0, 0);
+        Amount goal = new Amount(100, 0);
+        ArrayList<Transaction> transactions = new ArrayList<>();
+
+        Budget budget = new Budget(budgetName, month, income, expense, balance, goal, transactions);
         Assert.assertNotNull(budget);
     }
 }
