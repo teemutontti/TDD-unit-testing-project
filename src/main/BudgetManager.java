4ebd3fdb2c76ad7499586f76f2e460a66dcdcb95
Teemu Tontti <teemu.tontti.tt@gmail.com>
2023-11-23 15:53:52 +0200

    ../BudgetManager.java: (outputData & toJson): Add data output functionality.

--- src/main/BudgetManager.java
+++ src/main/BudgetManager.java
@@ -1,6 +1,8 @@
 package main;
 
 import java.util.*;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class BudgetManager {
     ArrayList<Budget> budgets;
@@ -14,6 +16,62 @@ public class BudgetManager {
     public void addBudget(String name, String month, Amount goal) {
         Budget budget = new Budget(name, month, goal);
         this.budgets.add(budget);
+    }
+
+    public void outputData() {
+        try {
+            FileWriter fw = new FileWriter("src/data.json");
+            fw.write(this.toJson());
+            fw.close();
+            System.out.println("Data written successfully!");
+        } catch (IOException e) {
+            System.err.println(e);
+            e.printStackTrace();
+        }
+    }
+
+    public String toJson() {
+        String jsonString = "[\n    {\n";
+
+        for (int i=0; i<this.budgets.size(); i++) {
+            Budget budget = this.budgets.get(i);
+
+            jsonString += "        \"id\": "+i+",\n";
+            jsonString += "        \"name\": \""+budget.getName()+"\",\n";
+            jsonString += "        \"month\": \""+budget.getMonth()+"\",\n";
+            jsonString += "        \"goal\": "+budget.getGoal()+",\n";
+            jsonString += "        \"transactions\": [";
+
+            if (budget.getTransactions().size() > 0) {
+                jsonString += "\n";
+                jsonString += "            {\n";
+                for (int j=0; j<budget.getTransactions().size(); j++) {
+                    Transaction transaction = budget.getTransactions().get(j);
+                    jsonString += "                \"id\": "+j+",\n";
+                    jsonString += "                \"name\": \""+transaction.getName()+"\",\n";
+                    jsonString += "                \"notes\": \""+transaction.getNotes()+"\",\n";
+                    jsonString += "                \"cents\": "+transaction.getCents()+",\n";
+                    jsonString += "                \"category\": \""+transaction.getCategory()+"\",\n";
+                    jsonString += "                \"isIncome\": \""+transaction.getIsIncome()+"\"\n";
+                    if (j < budget.getTransactions().size()-1) {
+                        jsonString += "            },\n";
+                        jsonString += "            {\n";
+                    } else {
+                        jsonString += "            }\n";
+                    }
+                }
+                jsonString += "        ]\n";
+            } else {
+                jsonString += "]\n";
+            }
+            if (i < this.budgets.size()-1) {
+                jsonString += "    },\n";
+                jsonString += "    {\n";
+            }
+        }
+        jsonString += "    }\n";
+        jsonString += "]";
+        return jsonString;
     }
 
     // Getters
