a011eb7112f052dbe43f66e4881645605c43d3d4
Teemu Tontti <teemu.tontti.tt@gmail.com>
2023-11-23 15:58:40 +0200

    ../TestBudgetManager.java: Add tests for data output.

--- src/test/TestBudgetManager.java
+++ src/test/TestBudgetManager.java
@@ -7,6 +7,8 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 import main.BudgetManager;
+import main.Budget;
+import main.Transaction;
 import main.Amount;
 
 @RunWith(JUnit4.class)
@@ -32,5 +34,114 @@ public class TestBudgetManager {
         BudgetManager budgetManager = new BudgetManager();
         budgetManager.addBudget("My Budget", "lokakuu", new Amount(10000, false));
         assertEquals(1, budgetManager.getBudgets().size());
+    }
+
+    @Test
+    public void testOutputData() {
+        BudgetManager budgetManager = new BudgetManager();
+        budgetManager.addBudget("My 1st Budget", "Lokakuu", new Amount(250, false));
+        Budget budget = budgetManager.getBudgets().get(0);
+        budget.addTransaction("Suklaa", "jotain", 500, false, "herkut", false);
+        budget.addTransaction("Tietokone", "jotain vaan", 45000, false, "elektroniikka", false);
+        budgetManager.outputData();
+    }
+
+    @Test
+    public void testToJsonNoTransactions() {
+        BudgetManager budgetManager = new BudgetManager();
+        budgetManager.addBudget("My 1st Budget", "Lokakuu", new Amount(250, false));
+        String expected = "";
+        expected += "[\n";
+        expected += "    {\n";
+        expected += "        \"id\": 0,\n";
+        expected += "        \"name\": \"My 1st Budget\",\n";
+        expected += "        \"month\": \"Lokakuu\",\n";
+        expected += "        \"goal\": 2.50,\n";
+        expected += "        \"transactions\": []\n";
+        expected += "    }\n";
+        expected += "]";
+        assertEquals(expected, budgetManager.toJson());
+    }
+    @Test
+    public void testToJsonWithTransactions() {
+        BudgetManager budgetManager = new BudgetManager();
+        budgetManager.addBudget("My 1st Budget", "Lokakuu", new Amount(250, false));
+        Budget budget = budgetManager.getBudgets().get(0);
+        budget.addTransaction("Suklaa", "jotain", 500, false, "herkut", false);
+        budget.addTransaction("Tietokone", "jotain vaan", 45000, false, "elektroniikka", false);
+        assertEquals(budget.getName(), "My 1st Budget");
+        String expected = "";
+        expected += "[\n";
+        expected += "    {\n";
+        expected += "        \"id\": 0,\n";
+        expected += "        \"name\": \"My 1st Budget\",\n";
+        expected += "        \"month\": \"Lokakuu\",\n";
+        expected += "        \"goal\": 2.50,\n";
+        expected += "        \"transactions\": [\n";
+        expected += "            {\n";
+        expected += "                \"id\": 0,\n";
+        expected += "                \"name\": \"Suklaa\",\n";
+        expected += "                \"notes\": \"jotain\",\n";
+        expected += "                \"cents\": 500,\n";
+        expected += "                \"category\": \"herkut\",\n";
+        expected += "                \"isIncome\": \"false\"\n";
+        expected += "            },\n";
+        expected += "            {\n";
+        expected += "                \"id\": 1,\n";
+        expected += "                \"name\": \"Tietokone\",\n";
+        expected += "                \"notes\": \"jotain vaan\",\n";
+        expected += "                \"cents\": 45000,\n";
+        expected += "                \"category\": \"elektroniikka\",\n";
+        expected += "                \"isIncome\": \"false\"\n";
+        expected += "            }\n";
+        expected += "        ]\n";
+        expected += "    }\n";
+        expected += "]";
+        assertEquals(expected, budgetManager.toJson());
+    }
+    @Test
+    public void testToJsonWithMultipleBudgets() {
+        BudgetManager budgetManager = new BudgetManager();
+        budgetManager.addBudget("My 1st Budget", "Lokakuu", new Amount(25000, false));
+        budgetManager.addBudget("My 2st Budget", "Marraskuu", new Amount(25000, false));
+        Budget budget = budgetManager.getBudgets().get(0);
+        budget.addTransaction("Suklaa", "jotain", 500, false, "herkut", false);
+        budget.addTransaction("Tietokone", "jotain vaan", 45000, false, "elektroniikka", false);
+        assertEquals(budget.getName(), "My 1st Budget");
+        String expected = "";
+        expected += "[\n";
+        expected += "    {\n";
+        expected += "        \"id\": 0,\n";
+        expected += "        \"name\": \"My 1st Budget\",\n";
+        expected += "        \"month\": \"Lokakuu\",\n";
+        expected += "        \"goal\": 250.00,\n";
+        expected += "        \"transactions\": [\n";
+        expected += "            {\n";
+        expected += "                \"id\": 0,\n";
+        expected += "                \"name\": \"Suklaa\",\n";
+        expected += "                \"notes\": \"jotain\",\n";
+        expected += "                \"cents\": 500,\n";
+        expected += "                \"category\": \"herkut\",\n";
+        expected += "                \"isIncome\": \"false\"\n";
+        expected += "            },\n";
+        expected += "            {\n";
+        expected += "                \"id\": 1,\n";
+        expected += "                \"name\": \"Tietokone\",\n";
+        expected += "                \"notes\": \"jotain vaan\",\n";
+        expected += "                \"cents\": 45000,\n";
+        expected += "                \"category\": \"elektroniikka\",\n";
+        expected += "                \"isIncome\": \"false\"\n";
+        expected += "            }\n";
+        expected += "        ]\n";
+        expected += "    },\n";
+        expected += "    {\n";
+        expected += "        \"id\": 1,\n";
+        expected += "        \"name\": \"My 2st Budget\",\n";
+        expected += "        \"month\": \"Marraskuu\",\n";
+        expected += "        \"goal\": 250.00,\n";
+        expected += "        \"transactions\": []\n";
+        expected += "    }\n";
+        expected += "]";
+        assertEquals(expected, budgetManager.toJson());
     }
 }
