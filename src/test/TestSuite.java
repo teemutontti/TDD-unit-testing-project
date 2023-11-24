eeb4cea2ad66ed1beffa7aaf68c55357e6d44bdd
Teemu Tontti <teemu.tontti.tt@gmail.com>
2023-11-24 03:50:16 +0200

    ../TestSuite.java: Add suite file.

---
+++ src/test/TestSuite.java
@@ -0,0 +1,16 @@
+package test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        TestAmount.class,
+        TestBudget.class,
+        TestBudgetManager.class,
+        TestTransaction.class
+})
+
+public class TestSuite {
+
+}
