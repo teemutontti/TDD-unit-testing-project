203c219e440ebebaf613414b9d653474aa101f7e
Teemu Tontti <teemu.tontti.tt@gmail.com>
2023-11-24 03:47:38 +0200

    ../TestTransaction.java: Edit code formatting.

--- src/test/TestTransaction.java
+++ src/test/TestTransaction.java
@@ -2,7 +2,6 @@ package test;
 
 import static org.junit.Assert.*;
 import org.junit.Test;
-//import org.junit.Ignore;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
@@ -24,11 +23,11 @@ public class TestTransaction {
         assertNotNull(transaction);
     }
 
-    //toString tests
+    // toString tests
     @Test
     public void testToString() {
         Transaction transaction = new Transaction("Tikkari", "", 105, false, "", false);
-        assertEquals("Tikkari, -1.05, "+currentDate, transaction.toString());
+        assertEquals("Tikkari, -1.05, " + currentDate, transaction.toString());
     }
 
     // formDate tests
@@ -40,30 +39,37 @@ public class TestTransaction {
 
     // Getter tests
     Transaction getterTestTransaction = new Transaction("Näyttö", "jotain", 10000, false, "elektroniikka", false);
+
     @Test
     public void testGetName() {
         assertEquals("Näyttö", getterTestTransaction.getName());
     }
+
     @Test
     public void testGetNotes() {
         assertEquals("jotain", getterTestTransaction.getNotes());
     }
+
     @Test
     public void testGetCents() {
         assertEquals(10000, getterTestTransaction.getCents());
     }
+
     @Test
     public void testGetAmount() {
         assertEquals("100.00", getterTestTransaction.getAmount());
     }
+
     @Test
     public void testGetCategory() {
         assertEquals("elektroniikka", getterTestTransaction.getCategory());
     }
+
     @Test
     public void testGetDate() {
         assertEquals(currentDate, getterTestTransaction.getDate());
     }
+
     @Test
     public void testGetIsIncome() {
         assertEquals(false, getterTestTransaction.getIsIncome());
