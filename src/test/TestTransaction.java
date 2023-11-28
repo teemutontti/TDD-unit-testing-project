fc2f1c8eabeccb56bf8aa514979bfa063ade0064
Teemu Tontti <teemu.tontti.tt@gmail.com>
2023-11-28 20:52:21 +0200

    ../TestTransaction.java: Remove unused imports.

--- src/test/TestTransaction.java
+++ src/test/TestTransaction.java
@@ -5,7 +5,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
-import main.Amount;
 import main.Transaction;
 
 import java.time.format.DateTimeFormatter;
