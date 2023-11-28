1f0a67d34fa379a9f50e9e71cc36b15d60d8daa5
Teemu Tontti <teemu.tontti.tt@gmail.com>
2023-11-28 20:43:55 +0200

    ../Transaction.java: (Transaction): Add cents over zero check.

--- src/main/Transaction.java
+++ src/main/Transaction.java
@@ -14,18 +14,22 @@ public class Transaction {
 
     // Constructor
     public Transaction(String name, String notes, int cents, boolean isNegative, String category, boolean isIncome) {
-        this.name = name;
-        this.notes = notes;
-        this.cents = cents;
-        this.amount = new Amount(cents, isNegative);
-        this.category = category; // TODO: Add modifiable category
-        this.isIncome = isIncome;
-        this.date = formDate();
+        if (cents > 0) {
+            this.name = name;
+            this.notes = notes;
+            this.cents = cents;
+            this.amount = new Amount(cents, isNegative);
+            this.category = category;
+            this.isIncome = isIncome;
+            this.date = formDate();
+        } else {
+            throw new IllegalArgumentException("Amount must be over zero!");
+        }
     }
 
     @Override
     public String toString() {
-        if(this.isIncome) {
+        if (this.isIncome) {
             return this.name + ", +" + this.amount.getAmount() + ", " + this.date;
         } else {
             return this.name + ", -" + this.amount.getAmount() + ", " + this.date;
@@ -43,21 +47,27 @@ public class Transaction {
     public String getName() {
         return this.name;
     }
+
     public String getNotes() {
         return this.notes;
     }
+
     public int getCents() {
         return this.cents;
     }
+
     public String getAmount() {
         return this.amount.getAmount();
     }
+
     public String getCategory() {
         return this.category;
     }
+
     public String getDate() {
         return this.date;
     }
+
     public boolean getIsIncome() {
         return this.isIncome;
     }
