26f7e60f7e963629a1c0e1a287cd109830e407be
Teemu Tontti <teemu.tontti.tt@gmail.com>
2023-11-28 20:45:16 +0200

    ../TestBudget.java: Edit formatting.

--- src/test/TestBudget.java
+++ src/test/TestBudget.java
@@ -33,6 +33,7 @@ public class TestBudget {
         assertEquals("250.00", budget.getBalance());
         assertEquals(1, budget.getTransactions().size());
     }
+
     @Test
     public void testAddMultipleTransactions() {
         Budget budget = new Budget("My Budget", "Lokakuu", new Amount(10000, false));
@@ -51,6 +52,7 @@ public class TestBudget {
         budget.addIncome(1000);
         assertEquals("10.00", budget.getIncome());
     }
+
     @Test
     public void testAddIncomeNegative() {
         Budget budget = new Budget("My Budget", "Lokakuu", new Amount(10000, false));
@@ -65,6 +67,7 @@ public class TestBudget {
         budget.addExpense(700);
         assertEquals("7.00", budget.getExpense());
     }
+
     @Test
     public void testAddExpenseNegative() {
         Budget budget = new Budget("My Budget", "Lokakuu", new Amount(10000, false));
@@ -80,6 +83,7 @@ public class TestBudget {
         budget.updateBalance();
         assertEquals("10.00", budget.getBalance());
     }
+
     @Test
     public void testUpdateBalanceNegative() {
         Budget budget = new Budget("My Budget", "Lokakuu", new Amount(10000, false));
@@ -91,45 +95,51 @@ public class TestBudget {
     /* --- Getter tests --- */
     @Test
     public void testGetBalance() {
-        Budget budget = new Budget("My Budget", "Lokakuu", new Amount(10000,false));
+        Budget budget = new Budget("My Budget", "Lokakuu", new Amount(10000, false));
         assertEquals("0.00", budget.getBalance());
     }
+
     @Test
     public void testGetIncome() {
-        Budget budget = new Budget("My Budget", "Lokakuu", new Amount(10000,false));
+        Budget budget = new Budget("My Budget", "Lokakuu", new Amount(10000, false));
         assertEquals("0.00", budget.getIncome());
     }
+
     @Test
     public void testGetExpense() {
-        Budget budget = new Budget("My Budget", "Lokakuu", new Amount(10000,false));
+        Budget budget = new Budget("My Budget", "Lokakuu", new Amount(10000, false));
         assertEquals("0.00", budget.getExpense());
     }
+
     @Test
     public void testGetName() {
-        Budget budget = new Budget("My Budget", "Lokakuu", new Amount(10000,false));
+        Budget budget = new Budget("My Budget", "Lokakuu", new Amount(10000, false));
         assertEquals("My Budget", budget.getName());
     }
+
     @Test
     public void testGetMonth() {
-        Budget budget = new Budget("My Budget", "Lokakuu", new Amount(10000,false));
+        Budget budget = new Budget("My Budget", "Lokakuu", new Amount(10000, false));
         assertEquals("Lokakuu", budget.getMonth());
     }
+
     @Test
     public void testGetGoal() {
-        Budget budget = new Budget("My Budget", "Lokakuu", new Amount(10000,false));
+        Budget budget = new Budget("My Budget", "Lokakuu", new Amount(10000, false));
         assertEquals("100.00", budget.getGoal());
     }
 
     /* --- Setter tests --- */
     @Test
     public void testSetName() {
-        Budget budget = new Budget("My Budget", "Lokakuu", new Amount(10000,false));
+        Budget budget = new Budget("My Budget", "Lokakuu", new Amount(10000, false));
         budget.setName("Teemu's Budget");
         assertEquals("Teemu's Budget", budget.getName());
     }
+
     @Test
     public void testSetMonth() {
-        Budget budget = new Budget("My Budget", "Lokakuu", new Amount(10000,false));
+        Budget budget = new Budget("My Budget", "Lokakuu", new Amount(10000, false));
         budget.setMonth("Marraskuu");
         assertEquals("Marraskuu", budget.getMonth());
     }
